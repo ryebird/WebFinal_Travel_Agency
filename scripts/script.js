f9a59a42f4802c3aec41edb9ae1565d591f48d3f
ryebird <itsguldana@gmail.com>
2023-06-16 18:54:36 +0600

    changes

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src')
+        document.querySelector('.about .video-container .video').src = src; 
+    }
+})
